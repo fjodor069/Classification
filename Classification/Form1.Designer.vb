fc964779d4f5fb2a86a9c22decd66f28a67955c7
fjodor069 <napoleon@hetnet.nl>
2015-10-17 19:36:02 +0200

    Classification source in VB
    Visual Basic program to determine the classification of a pressure
    vessel in Chinese Code Rules

---
+++ Classification/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.Window
+        Me.PictureBox1.Location = New System.Drawing.Point(286, 72)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(440, 317)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(26, 239)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(190, 34)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Determine Category"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(23, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(382, 22)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Categorisation diagram of Pressure Vessels"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(24, 154)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 16)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Pressure (MPa)"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(23, 188)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 16)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Volume (L)"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(145, 150)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(145, 184)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(24, 101)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 16)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Medium group"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(155, 98)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton1.TabIndex = 8
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "group 1"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(155, 121)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(61, 17)
+        Me.RadioButton2.TabIndex = 9
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "group 2"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(27, 293)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(200, 96)
+        Me.RichTextBox1.TabIndex = 10
+        Me.RichTextBox1.Text = ""
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(29, 43)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(520, 13)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "according to TSG R0004-2009 Supervision Regulations on Safety Technology for Stat" & _
+    "ionary Pressure Vessel"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(751, 425)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "TSG Category"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Classification/Form1.vb
@@ -0,0 +1,312 @@
+﻿Public Class Form1
+
+    ''' <summary>
+    ''' 'this class implements the classification for pressure vessels
+    ''' according to the Chinese Manufacturing Licensing of Pressure Vessels;
+    ''' TSG R0004-2009 Supervision Regulations on Safety Technology for Stationary Pressure Vessel
+    ''' fig. A1 and A2
+    ''' </summary>
+    ''' <remarks></remarks>
+    Enum type_medium
+        medium1
+        medium2
+    End Enum
+
+    Enum type_category
+        class0
+        classI
+        classII
+        classIII
+    End Enum
+
+    Enum type_licensing
+        A1
+        A2
+        D2
+        D1
+        unlicensed
+    End Enum
+
+    Private P As Double
+    Private V As Double
+
+    Private mediumgroup As type_medium
+    Private vesselcategory As type_category
+    Private licensinglevel As type_licensing
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'form load
+
+        RadioButton1.Checked = True
+        RadioButton2.Checked = False
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        'category button
+
+        'get the input
+        Try
+            P = Double.Parse(TextBox1.Text)
+            V = Double.Parse(TextBox2.Text)
+        Catch ex As Exception
+            MessageBox.Show("You must enter a value")
+            Return
+        End Try
+        'P = 1
+        'V = 1
+       
+        If RadioButton1.Checked Then
+            mediumgroup = type_medium.medium1
+        Else
+            mediumgroup = type_medium.medium2
+        End If
+
+        If mediumgroup = type_medium.medium1 Then
+            vesselcategory = DetermineCategoryA1(P, V)
+        Else
+            vesselcategory = DetermineCategoryA2(P, V)
+        End If
+
+        Select Case vesselcategory
+
+            Case type_category.class0
+                licensinglevel = type_licensing.unlicensed
+
+            Case type_category.classI
+                licensinglevel = type_licensing.D1
+
+            Case type_category.classII
+                licensinglevel = type_licensing.D2
+
+            Case type_category.classIII
+                If P < 10 Then
+                    licensinglevel = type_licensing.A2
+                Else
+                    licensinglevel = type_licensing.A1
+                End If
+
+
+        End Select
+
+        PrintResults()
+
+        DrawPicture()
+
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged, RadioButton2.CheckedChanged
+
+        Dim rb As RadioButton = TryCast(sender, RadioButton)
+
+        If rb Is Nothing Then Return
+
+        If RadioButton1.Checked Then
+            RadioButton2.Checked = False
+        End If
+        If RadioButton2.Checked Then
+            RadioButton1.Checked = False
+        End If
+
+
+    End Sub
+
+    'teken de grafiek in de picturebox
+    Private Sub DrawPicture()
+
+
+        Dim g As Graphics = PictureBox1.CreateGraphics()
+        Dim rect As Rectangle
+        Dim margin As Integer = 20
+        Dim numX As Integer = 10
+        Dim numY As Integer = 4
+
+        ' g.DrawLine(Pens.Black, 10, 10, 20, 20)
+        rect = New Rectangle(0 + margin, 0 + margin, PictureBox1.Width - margin * 2, PictureBox1.Height - margin * 2)
+
+        g.DrawRectangle(Pens.Blue, rect)
+
+        'zet de origin op een ander punt
+        g.TranslateTransform(margin, PictureBox1.Height - margin)
+
+        Dim p1 As New Pen(Color.Black, 2)
+        'teken de x -as 
+        Dim distX As Integer = PictureBox1.Width - margin * 2
+        Debug.Print("distX = " & distX)
+
+        g.DrawLine(p1, 0, 0, distX, 0)
+
+        'teken de y -as
+        Dim distY = PictureBox1.Height - margin * 2
+        Debug.Print("distY = " & distY)
+        g.DrawLine(p1, 0, 0, 0, -distY)
+
+        'teken major gridlines verticaal
+        For i As Integer = 0 To numX - 1
+            Dim xdist As Integer = i / numX * distX
+            g.DrawLine(Pens.Blue, xdist, 0, xdist, -distY)
+        Next
+
+        'teken minor gridlines verticaal
+        For i As Integer = 0 To numX - 1
+            Dim xdist As Integer = i / numX * distX
+            For j As Integer = 1 To 9
+
+                Dim logdist As Integer = Math.Log10(j) * (distX / numX) + xdist
+                g.DrawLine(Pens.Black, logdist, 0, logdist, -distY)
+
+            Next
+        Next
+
+        'teken major gridlines horizontaal
+        For i As Integer = 0 To numY - 1
+            Dim ydist As Integer = i / numY * distY
+            g.DrawLine(Pens.Blue, 0, -ydist, distX, -ydist)
+        Next
+
+        'teken minor gridlines horizontaal
+        For i As Integer = 0 To numY - 1
+            Dim ydist As Integer = i / numY * distY
+            For j As Integer = 1 To 9
+
+                Dim logdist As Integer = Math.Log10(j) * (distY / numY) + ydist
+                g.DrawLine(Pens.Black, 0, -logdist, distX, -logdist)
+
+            Next
+        Next
+
+        'teken het punt P,V
+
+
+        Dim myPoint As New Point
+
+        myPoint.X = Int(Math.Log10(V) / (numX - 1) * distX)
+        myPoint.Y = -Int(Math.Log10(P) / (numY - 1) * distY)
+        Debug.Print(myPoint.ToString)
+
+        g.FillEllipse(Brushes.Crimson, myPoint.X, myPoint.Y, 5, 5)
+       
+    End Sub
+
+
+    Private Sub PrintResults()
+
+        Dim s As String
+
+        RichTextBox1.Clear()
+        RichTextBox1.AppendText("Pressure P = " & P & vbCrLf)
+        RichTextBox1.AppendText("Volume V = " & V & vbCrLf)
+
+        If mediumgroup = type_medium.medium1 Then
+            s = "medium 1"
+        Else
+            s = "medium 2"
+        End If
+        RichTextBox1.AppendText("Medium group = " & s & vbCrLf)
+
+        RichTextBox1.AppendText("Vessel category = " & vesselcategory.ToString & vbCrLf)
+
+        RichTextBox1.AppendText("Licensing level = " & licensinglevel.ToString & vbCrLf)
+        'Select Case licensinglevel
+
+        '    Case type_licensing.A1
+        '        RichTextBox1.AppendText(licensinglevel.ToString & vbCrLf)
+        '    Case type_licensing.A2
+        '        RichTextBox1.AppendText("group = " & s & vbCrLf)
+        '    Case type_licensing.D1
+        '        RichTextBox1.AppendText("Medium group = " & s & vbCrLf)
+        '    Case type_licensing.D2
+        '        RichTextBox1.AppendText("Medium group = " & s & vbCrLf)
+
+
+
+        'End Select
+
+
+
+    End Sub
+
+    Private Function DetermineCategoryA1(ByVal P As Double, ByVal V As Double) As type_category
+
+        Dim PV As Double
+        Dim myCategory As type_category
+
+        PV = P * V
+
+        'for medium group 1
+        If (PV < 2.5 And V < 25) Or (P < 0.1) Then
+            myCategory = type_category.class0
+
+        Else
+            If (V < 25) Then
+                myCategory = type_category.classI
+            Else
+                If (P < 10 And V < 5000.0) Then
+                    myCategory = type_category.classII
+                Else
+                    If (P < 10 And PV < 50000.0 And V < 31250) Then
+                        myCategory = type_category.classII
+                    Else
+                        If (P < 1.6 And V < 600000.0) Then
+                            myCategory = type_category.classII
+                        Else
+                            If (P < 1.6 And PV < 1000000.0 And V < 10000000.0) Then
+                                myCategory = type_category.classII
+                            Else
+                                myCategory = type_category.classIII
+                            End If
+                        End If
+                    End If
+
+                End If
+            End If
+        End If
+
+
+        Return myCategory
+
+
+    End Function
+
+    Private Function DetermineCategoryA2(ByVal P As Double, ByVal V As Double) As type_category
+
+        Dim PV As Double
+        Dim myCategory As type_category
+
+        PV = P * V
+
+        'for medium group 2
+        If (PV < 2.5 And V < 25) Or (P < 0.1) Then
+            myCategory = type_category.class0
+
+        Else
+            If (V < 25) Then
+                myCategory = type_category.classI
+            Else
+                If (P < 1.6 And V < 3125000.0) Then
+                    myCategory = type_category.classI
+                Else
+                    If (P < 1.6 And PV < 5000000.0 And V < 500000.0) Then
+                        myCategory = type_category.classI
+                    Else
+                        If (P < 10 And V < 50000.0) Then
+                            myCategory = type_category.classII
+                        Else
+                            If (P < 10 And PV < 50000.0 And V < 312500) Then
+                                myCategory = type_category.classII
+                            Else
+                                myCategory = type_category.classIII
+                            End If
+                        End If
+                    End If
+
+                End If
+            End If
+        End If
+
+        Return myCategory
+
+    End Function
+
+End Class
